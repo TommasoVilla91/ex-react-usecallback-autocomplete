62d928ce2af6d6fb6f5dba60afb28b59809324b8
TommasoVilla91 <tommasovilla@yahoo.it>
2025-03-27 11:39:29 +0100

    feat: milestone 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,58 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const [inputValue, setInputValue] = useState("");
   const [products, setProducts] = useState([]);
+  const [show, setShow] = useState(false);
 
+  const getProducts = async() => {
+    if(inputValue.trim() !== "") {
+      try {
+        const response = await fetch(`https://boolean-spec-frontend.vercel.app/freetestapi/products?search=${inputValue}`);
+        const [obj] = await response.json();
+        setProducts([...products, obj]);
+      } catch(err) {
+        console.error(err);
+      } finally {
+        setShow(true);
+      }
+    } else {
+      setShow(false)
+    }
+  }
 
+  useEffect(() => {
+    getProducts();
+  }, [inputValue])
+
+  
   return (
     <main>
       <div className="container">
-        <div>
-          <input 
-            type="text"
-            value={inputValue} 
-            onChange={(e) => e.target.value}
-          />
-        </div>
+        <section>
+          <div>
+            <input 
+              type="text"
+              value={inputValue} 
+              onChange={(e) => setInputValue(e.target.value)}
+            />
+          </div>
+        </section>
+
+        <section>
+          <div>
+            {show && (
+              <details>
+                <summary>Prodotti</summary>
+                {products.map((p, i) => (
+                  <div key={i}>
+                    <h3>{p.name}</h3>
+                  </div>
+                ))}
+              </details>
+            )}
+          </div>
+        </section>
       </div>
       
     </main>
